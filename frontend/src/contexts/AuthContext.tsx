b65387287583824c44c9149b423b304a44164a7a
Boudewijn Aasman <boudeyz@gmail.com>
2025-08-01 14:23:07 -0400

    Add include to auth context

--- frontend/src/contexts/AuthContext.tsx
+++ frontend/src/contexts/AuthContext.tsx
@@ -97,6 +97,7 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
         headers: {
           'Content-Type': 'application/json',
         },
+        credentials: 'include',
         body: JSON.stringify({
           login: usernameOrEmail,
           password: password,
